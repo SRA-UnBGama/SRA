7a8a0c5c4cb39aa20fcf27032de61165e643d637
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-08-13 08:16:42 -0300

    Refactoring edit atendimento

--- app/helpers/atendimento_helper.rb
+++ app/helpers/atendimento_helper.rb
@@ -3,9 +3,16 @@ module AtendimentoHelper
 
   def check_place
   	place_name = place_client("none")
-    place_selected =  Place.where(active: true, name: place_name.chomp)
+    place_selected =  Place.where(active: true, name: place_name)
    
 
     place_selected
+  end
+
+  def check_atendimento_place(place)
+  	place_name = Place.find_by_id(place).name
+  	place_selected = Place.where(active: true, name: place_name)
+
+  	place_selected
   end
 end
